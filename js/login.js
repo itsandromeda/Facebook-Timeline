fe775fa58f64e7d19b8e63ebec96590875442ece
itsandromeda <itsandromeda@gmail.com>
2017-03-09 22:57:21 -0500

    Users & validation process

--- js/login.js
+++ js/login.js
@@ -1,28 +1,56 @@
-/*global document, alert*/
+/*global document, window*/
 /*jslint eqeq: true*/
-var logIn = document.getElementById("log-in"),
-    email,
-    password,
-    emailError = document.getElementById("error-a"),
-    pwError = document.getElementById("error-b"),
-    validEmail = /\S+@\S+\.\S+/;
-
-logIn.addEventListener("click", function () {
+window.addEventListener('load', function () {
     "use strict";
-    email = document.getElementById("email").value;
-    password = document.getElementById("pw").value;
-    
-    if (email === "") {
-        emailError.innerHTML = "*Este campo no puede estar vacío.";
-    } else if (!validEmail.test(email)) {
-        emailError.innerHTML = "*Por favor ingrese un email válido.";
-    } else {
-        emailError.innerHTML = "";
-    }
-    
-    if (password.length < 6) {
-        pwError.innerHTML = "*Por favor ingrese un password válido.";
-    } else {
-        pwError.innerHTML = "";
-    }
+    var logIn = document.getElementById('log-in');
+
+    logIn.addEventListener('click', function () {
+        var email = document.getElementById('email').value,
+            password = document.getElementById('pw').value,
+            emailError = document.getElementById('error-a'),
+            pwError = document.getElementById('error-b'),
+            users,
+            i;
+
+        users = [{
+            id: "itsandromeda",
+            email: "itsandromeda@gmail.com",
+            password: "itsandromeda",
+            timeline: "index.html?id=itsandromeda"
+        }, {
+            id: "ajasenka",
+            email: "ajasenka@gmail.com",
+            password: "ajasenka",
+            timeline: "index.html?id=ajasenka"
+        }];
+
+        for (i = 0; i < users.length; i += 1) {
+            if (email == users[0].email && password == users[0].password) {
+                window.location = users[0].timeline;
+            }
+            if (email == users[1].email && password == users[1].password) {
+                window.location = users[1].timeline;
+            }
+            /*EMAIL VALIDATION*/
+            if (email === "") {
+                emailError.innerHTML = "This field can't be blank.";
+            }
+            if (email !== users[0].email || email !== users[1].email) {
+                emailError.innerHTML = "This user doesn't exist.";
+            }
+            if (email == users[0].email || email == users[1].email) {
+                emailError.innerHTML = "";
+            }
+            /*PASSWORD VALIDATION*/
+            if (password === "") {
+                pwError.innerHTML = "This field can't be blank.";
+            }
+            if (password !== users[0].password || password !== users[1].password) {
+                pwError.innerHTML = "The password is incorrect.";
+            }
+            if (password == users[0].password || password == users[1].password) {
+                pwError.innerHTML = "";
+            }
+        }
+    });
 });
